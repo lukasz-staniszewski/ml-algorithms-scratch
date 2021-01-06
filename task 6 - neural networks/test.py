cdfd64454aed63cba7bb48530c964492cc9f48df
Łukasz Piotr Staniszewski <l.staniszewski@stud.elka.pw.edu.pl>
2021-01-06 05:40:12 +0100

    nearly ended matigit add .git add .

--- task 6 - neural networks/test.py
+++
@@ -1,6 +0,0 @@
-import network
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-training_data = list(training_data)
-net = network.Network([784, 800, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
